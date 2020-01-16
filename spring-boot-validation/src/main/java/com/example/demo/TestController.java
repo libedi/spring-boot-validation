b1fc7bcdbe01a9add5e84b7ed1f222d297fa6abf
Sangjun, Park <libedi@gmail.com>
2020-01-16 23:45:09 +0900

    Use MessageSource

--- spring-boot-validation/src/main/java/com/example/demo/TestController.java
+++ spring-boot-validation/src/main/java/com/example/demo/TestController.java
@@ -2,15 +2,23 @@ package com.example.demo;
 
 import javax.validation.Valid;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+	
+	private final MessageSource messageSource;
+	
+	public TestController(final MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	@GetMapping("/test")
 	public void testValidMsg(@Valid TestModel model) {
-		
+		this.messageSource.getMessage("valid.not-null", null, LocaleContextHolder.getLocale());
 	}
 	
 }
